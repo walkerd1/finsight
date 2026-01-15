d4af183b22ae04bd16fb7bf0d180c2ebca697cd3
@walkerd1 <dustinwa1989@gmail.com>
2026-01-15 10:20:04 -0700

    Clean Up
    Cleaned up Repo to reflect a Spring application instead of a Notion integration. Cleared out all related files to this change.

--- config/runtime_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER IF NOT EXISTS 'finsight_runtime'@'localhost' IDENTIFIED BY 'f1n31ght_runt1m3!';
-
-GRANT
-  SELECT, INSERT, UPDATE, DELETE
-ON finsight.* TO 'finsight_runtime'@'localhost';
-
-FLUSH PRIVILEGES;
--- policy/retention.rego
+++
@@ -1,7 +0,0 @@
-package finsight.retention
-
-deny[msg] {
-  input.kind == "Bucket"
-  not input.spec.ttl_days
-  msg := sprintf("Bucket %s missing ttl_days", [input.metadata.name])
-}
